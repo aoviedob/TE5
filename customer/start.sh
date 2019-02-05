da146f3460aca47a7188c7b2184df0c5b07c2fc4
Andres <a.oviedo.b@gmail.com>
2019-02-04 21:07:16 -0600

    FEAT: [Customer] Create Nodejs/ExpressJs Server #28

---
+++ customer/start.sh
@@ -0,0 +1,19 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+yarn install
+
+if [ ! "$(docker ps -q -f name=customer)" ]; then
+  if [ "$(docker ps -aq -f status=exited -f name=customer)" ]; then
+    docker rm customer
+  fi
+  
+  docker rmi customer
+  docker build --tag=customer .
+  docker run -d --name customer -p 3030:3030 --net="host" customer
+fi
+
+./scripts/create_schema.sh;
+npm run migrate;
+./scripts/create_test_schema.sh;
+npm run migrate-test;
