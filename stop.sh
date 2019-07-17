d08979051d0428a44a7636faf6d2b7767fa74da6
Andres Oviedo Brenes <andres.oviedo@avantica.net>
2019-07-17 15:35:54 -0600

    FIX: stop script

--- stop.sh
+++ stop.sh
@@ -1,7 +1,27 @@
-docker stop auth || true
-docker stop customer
-docker stop event
-docker stop ticket
-docker stop payment
-docker stop postgres
-docker stop comm
+if [ ! "$(docker ps -q -f name=auth)" ]; then
+  docker stop auth
+fi
+
+if [ ! "$(docker ps -q -f name=customer)" ]; then
+  docker stop customer
+fi
+
+if [ ! "$(docker ps -q -f name=event)" ]; then
+  docker stop event
+fi
+
+if [ ! "$(docker ps -q -f name=ticket)" ]; then
+  docker stop ticket
+fi
+
+if [ ! "$(docker ps -q -f name=payment)" ]; then
+  docker stop payment
+fi
+
+if [ ! "$(docker ps -q -f name=postgres)" ]; then
+  docker stop postgres
+fi
+
+if [ ! "$(docker ps -q -f name=comm)" ]; then
+  docker stop comm
+fi
