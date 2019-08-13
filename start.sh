88382130ce532c29b307dc344c9126203a2f8e51
Andres Oviedo Brenes <andres.oviedo@avantica.net>
2019-08-13 16:20:36 -0600

    FIX: Removed character

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ if [ ! "$(docker ps -q -f name=postgres)" ]; then
 else
   docker run -d --name postgres -p 5432:5432 --net="host" -v pgdb:/data postgres:9.6
 fi
-`
+
 sleep 30
 docker exec -i postgres psql -h localhost -p 5432 -U postgres -c "ALTER USER postgres WITH PASSWORD 'mb|33_<C<kl&A*df.c8%*';"
 
