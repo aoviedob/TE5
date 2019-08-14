7b60a2787fc2450446a6840906cd07252f60bfbb
Andres Oviedo Brenes <andres.oviedo@avantica.net>
2019-08-14 09:24:10 -0600

    FIX: Scripts

--- event/scripts/seed_data.sh
+++ event/scripts/seed_data.sh
@@ -1,3 +1,3 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
-docker exec -it postgres psql -h localhost -p 5432 -U postgres -d postgres -c "$(cat ../database/seed_data.sql)"
+docker exec -i postgres psql -h localhost -p 5432 -U postgres -d postgres -c "$(cat ../database/seed_data.sql)"
