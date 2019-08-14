416713d78e0747f2ef39854478031602e6b51ce7
Andres Oviedo Brenes <andres.oviedo@avantica.net>
2019-08-14 14:36:19 -0600

    FIX: DB incon

--- auth/database/migrations/20181224141626_create_role_by_user_table.js
+++ auth/database/migrations/20181224141626_create_role_by_user_table.js
@@ -4,8 +4,8 @@ exports.up = async dbConnection =>
   await dbConnection.raw(`
     CREATE TABLE IF NOT EXISTS :schema:.role_by_user(
       id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-      user_id uuid not null REFERENCES :schema:.user(id) ,
-      role_id uuid not null REFERENCES :schema:.role(id),
+      user_id uuid not null,
+      role_id uuid not null,
       created_at timestamp not null,
       updated_at timestamp not null,
       UNIQUE (user_id, role_Id)
