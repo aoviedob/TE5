6eb2f231166d518e4356e2d79d67e5104e7091c4
Andres <a.oviedo.b@gmail.com>
2019-08-13 19:48:04 -0600

    FIX: Changed the auth config

--- client/config.js
+++ client/config.js
@@ -4,7 +4,7 @@ module.exports = {
   eventServiceDomain: getEnvVariable('EVENT_SERVICE_DOMAIN', 'http://localhost:3050'),
   commServiceDomain: getEnvVariable('COMM_SERVICE_DOMAIN', 'http://localhost:3065'),
   ticketServiceDomain: getEnvVariable('TICKET_SERVICE_DOMAIN', 'http://localhost:3060'),
-  authServiceDomain: getEnvVariable('AUTH_SERVICE_DOMAIN', 'http://localhost:3000'),
+  authServiceDomain: getEnvVariable('AUTH_SERVICE_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:3000'),
   customerServiceDomain: getEnvVariable('CUSTOMER_SERVICE_DOMAIN', 'http://localhost:3030'),
   ticketSocketIODomain: getEnvVariable('TICKET_SOCKET_DOMAIN', 'http://localhost:8099'),
   customerSocketIODomain: getEnvVariable('CUSTOMER_SOCKET_DOMAIN', 'http://localhost:8090'),
