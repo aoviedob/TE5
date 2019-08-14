64af0d7f2dd3e4a86fd5fbcce0b5c3d370ead4ea
Andres <a.oviedo.b@gmail.com>
2019-08-13 20:25:36 -0600

    FEAT: Start

--- client/config.js
+++ client/config.js
@@ -1,13 +1,13 @@
 const { getEnvVariable } = require('./helpers/environment');
 
 module.exports = {
-  eventServiceDomain: getEnvVariable('EVENT_SERVICE_DOMAIN', 'http://localhost:3050'),
-  commServiceDomain: getEnvVariable('COMM_SERVICE_DOMAIN', 'http://localhost:3065'),
-  ticketServiceDomain: getEnvVariable('TICKET_SERVICE_DOMAIN', 'http://localhost:3060'),
+  eventServiceDomain: getEnvVariable('EVENT_SERVICE_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:3050'),
+  commServiceDomain: getEnvVariable('COMM_SERVICE_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:3065'),
+  ticketServiceDomain: getEnvVariable('TICKET_SERVICE_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:3060'),
   authServiceDomain: getEnvVariable('AUTH_SERVICE_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:3000'),
-  customerServiceDomain: getEnvVariable('CUSTOMER_SERVICE_DOMAIN', 'http://localhost:3030'),
-  ticketSocketIODomain: getEnvVariable('TICKET_SOCKET_DOMAIN', 'http://localhost:8099'),
-  customerSocketIODomain: getEnvVariable('CUSTOMER_SOCKET_DOMAIN', 'http://localhost:8090'),
+  customerServiceDomain: getEnvVariable('CUSTOMER_SERVICE_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:3030'),
+  ticketSocketIODomain: getEnvVariable('TICKET_SOCKET_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:8099'),
+  customerSocketIODomain: getEnvVariable('CUSTOMER_SOCKET_DOMAIN', 'http://te5.centralus.cloudapp.azure.com:8090'),
   authSystemLoginCredentials: {
     email: getEnvVariable('AUTH_SYSTEM_LOGIN_USER', 'andres+system@gmail.com'),
     password: getEnvVariable('AUTH_SYSTEM_LOGIN_PASSWORD', 'secret'),
--- event/start.sh
+++ event/start.sh
@@ -1,6 +1,3 @@
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
 yarn install
 
 if [ ! "$(docker ps -q -f name=event)" ]; then
